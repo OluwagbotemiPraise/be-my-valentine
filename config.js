4a336aba91c57dc3ed6b4324ab3bb8d80fa68d57
OluwagbotemiPraise <praisefaboya2015@gmail.com>
2026-02-04 10:31:34 +0000

    Update config.js

--- config.js
+++ config.js
@@ -78,7 +78,7 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://res.cloudinary.com/dncywqfpb/video/upload/v1738399057/music_qrhjvy.mp3", // Music streaming URL
+        musicUrl: "https://res.cloudinary.com/dtekm22n7/video/upload/v1770200993/Whitney_Houston_-_I_Will_Always_Love_You_Official_4K_Video_rwehym.mp3", // Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
